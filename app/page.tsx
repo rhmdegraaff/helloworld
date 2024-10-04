80a1b6220af18ee108e44902f281a06a05f0015f
Ronald de Graaff | VCS Observation <ronald.de.graaff@vcsobservation.com>
2024-10-04 14:42:29 +0200

    commited by me

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,10 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li>Hello Ronald</li>
+
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{""}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
